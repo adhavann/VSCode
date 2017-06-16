6b6818f8ea7a606ef2b3c8e317dbb6ec47d00a87
Adhavann <user.nme="Adhavann>
2017-06-16 18:39:36 -0500

    getting data and store it in html

--- nodeApp.js
+++ nodeApp.js
@@ -1,2 +1,48 @@
 console.log("Hello I'm here");
-console.log("First Commit ");
+console.log("Creating the http server");
+var http=require('http');
+var fs=require('fs');
+//var request=require('request');
+/*var server=http.createServer(function(req,response){
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.end("Hello world");
+
+*/    // get the name of the person
+    process.stdout.write("Enter the name of the person ");
+    var username="";
+    process.stdin.on("data",function(input){
+        username=input;
+    });
+    //create the hostname
+    var hostDetails={
+        hostname:"en.wikipedia.org",
+        port:443,
+        path:`/wiki/${username}`,
+        method:"GET"
+    };
+//    response.end(function(respic){
+    var responseBody="";
+        http.request(hostDetails,function(res){
+                res.setEncoding("UTF-8");
+                if(res.statusCode==200){
+
+                    res.once("data",function(chunk){
+                        console.log("Data started flowing in!");
+                    });
+
+                    res.on("data",function(chunk){
+                        responseBody+=chunk;
+                        console.log(`Collected ${chunk.length}`);
+                    });
+
+                    res.on("end",function(chunk){
+                        process.stdout.write("Doneeee!");
+                        fs.createWriteStream("myHtml.html",responseBody,function(done){
+                            process.stdout.write("Your data stored in the data file!");
+                        });
+                    });
+                }
+        });
+/*   });
+}).listen(8000);
+*/
