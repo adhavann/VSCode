602b3bdce02ea668eae30124e16153c890d656e1
Adhavann <user.nme="Adhavann>
2017-06-18 22:12:13 -0500

    Download the data for the mentoned name and store it in HTML

--- nodeApp.js
+++ nodeApp.js
@@ -1,7 +1,7 @@
 console.log("Hello I'm here");
 console.log("Creating the http server");
-var http=require('http');
-var fs=require('fs');
+var https=require("https");
+var fs=require("fs");
 //var request=require('request');
 /*var server=http.createServer(function(req,response){
     response.writeHead(200,{"Content-Type":"text/plain"});
@@ -9,40 +9,65 @@ var fs=require('fs');
 
 */    // get the name of the person
     process.stdout.write("Enter the name of the person ");
-    var username="";
-    process.stdin.on("data",function(input){
+    var username="George_Washington";
+    var hostDetails="";
+    process.stdin.once('data',function(input){
         username=input;
+        console.log("Got data"+username);
+        hostDetails=
+        {
+            host:"en.wikipedia.org",
+            port:443,
+            path:"/wiki/"+username.toString().trim().replace(" ","_"),
+            method:"GET"
+        };
+        console.log(hostDetails.path);
+        var re=https.request(hostDetails,function(res){
+            res.setEncoding("UTF-8");
+            console.log(res.statusCode);
+            // if(res.statusCode==200){
+                    res.once('data',function(chunk){
+                        console.log("Data started flowing in!");
+                        process.stdout.write("yeahhhh");
+                    });
+                    res.on('data',function(chunk){
+                        responseBody+=chunk;
+                        //console.log("Collected"+ chunk.length);
+                    });
+                    res.on('end',function(chunk){
+                        process.stdout.write("Doneeee!");
+                        var file=fs.createWriteStream("myHtml.html");
+                        file.write(responseBody);
+                    });
+                // }
+        });
+        re.on('error',function(error){
+                    console.log("error "+error);
+        });
+        re.end();
+        // process.close();
     });
-    //create the hostname
-    var hostDetails={
-        hostname:"en.wikipedia.org",
-        port:443,
-        path:`/wiki/${username}`,
-        method:"GET"
-    };
 //    response.end(function(respic){
     var responseBody="";
-        http.request(hostDetails,function(res){
-                res.setEncoding("UTF-8");
-                if(res.statusCode==200){
-
-                    res.once("data",function(chunk){
-                        console.log("Data started flowing in!");
-                    });
-
-                    res.on("data",function(chunk){
-                        responseBody+=chunk;
-                        console.log(`Collected ${chunk.length}`);
-                    });
-
-                    res.on("end",function(chunk){
-                        process.stdout.write("Doneeee!");
-                        fs.createWriteStream("myHtml.html",responseBody,function(done){
-                            process.stdout.write("Your data stored in the data file!");
-                        });
-                    });
-                }
+    var firstFileBody="";
+    console.log(username);
+    https.get({
+        host:"www.google.com",
+        path:"/"
+    },function(data1){
+        data1.setEncoding("UTF-8");
+        data1.on('data',function(chunk){
+            //console.log(chunk);
+            firstFileBody+=chunk;
         });
+        data1.on('end',function(done){
+            var firstFile=fs.createWriteStream("firstGet.html");
+            firstFile.write(firstFileBody);
+
+        });
+    });
+    
+
 /*   });
 }).listen(8000);
 */
